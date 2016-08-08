24e423a9642b5e8bc348a45bbdaa7b937504da50
SCNUzy <975135274@qq.com>
2016-08-08 20:23:12 +0800

    啦啦啦

--- app/src/main/java/zy/chasegoddness/ui/view/SentimentBar.java
+++ app/src/main/java/zy/chasegoddness/ui/view/SentimentBar.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 
 /**
@@ -11,6 +12,18 @@ import android.view.View;
  * 情感分析显示正负面百分比的饼状图
  */
 public class SentimentBar extends View {
+
+    /**
+     * 控件默认的宽度
+     */
+    private final static int DEFAULT_RADIUS = 200;
+    /**
+     * 饼状图的外围半径 即是控件的宽度的1/2
+     */
+    private int radius_outside;
+    private int radius_inside;
+
+    
 
     public SentimentBar(Context context) {
         super(context);
@@ -29,5 +42,12 @@ public class SentimentBar extends View {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
-
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int minRadius = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, DEFAULT_RADIUS, getResources().getDisplayMetrics());
+        int width = getDefaultSize(minRadius, widthMeasureSpec);
+        int height = getDefaultSize(minRadius, heightMeasureSpec);
+        radius_outside = Math.min(width, height) / 2;
+        setMeasuredDimension(radius_outside * 2, radius_outside * 2);
+    }
 }
