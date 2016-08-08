fdd20e31c7791989418000ebed54f27d661ea8c9
SCNUzy <975135274@qq.com>
2016-08-08 19:48:48 +0800

    增加EmotionAnalyze的presenter

---
+++ app/src/main/java/zy/chasegoddness/model/AutoReplyModel.java
@@ -0,0 +1,37 @@
+package zy.chasegoddness.model;
+
+import rx.Observable;
+
+/**
+ * @author wsd
+ *         自动回复功能
+ */
+public class AutoReplyModel {
+    private AutoReplyModel() {
+    }
+
+    public Observable<String> getReply1(String content) {
+        //TODO: 获取自动回复
+        return null;
+    }
+
+    public Observable<String> getReply2(String content) {
+        //TODO: 获取自动回复
+        return null;
+    }
+
+    public Observable<String> getReply3(String content) {
+        //TODO: 获取自动回复
+        return null;
+    }
+
+    /**
+     * 获取多条自动回复内容
+     */
+    public Observable<String> getReply(String content) {
+        Observable<String> reply1 = getReply1(content);
+        Observable<String> reply2 = getReply2(content);
+        Observable<String> reply3 = getReply3(content);
+        return Observable.merge(reply1, reply2, reply3);
+    }
+}
